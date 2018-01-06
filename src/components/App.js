487bce8b13415b2c49edcccf109937eec59cd314
joseph_ctu <sephxd1234@gmail.com>
2018-01-06 10:51:00 +0100

    test: verify that App renders a form and its submit button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Form, Button, FormControl } from "react-bootstrap";
 
 class App extends Component {
   render() {
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,4 +8,27 @@ describe("App", () => {
   it("renders correctly", () => {
     expect(app).toMatchSnapshot();
   });
+
+  it("renders `My Notes` title", () => {
+    expect(app.find(".title").exists()).toBe(true);
+    expect(app.find(".title").text()).toEqual("My Notes");
+  });
+
+  it("renders a bootstrap Form", () => {
+    expect(app.find("Form").exists()).toBe(true);
+  });
+
+  it("renders a bootstrap FormControl", () => {
+    expect(app.find("FormControl").exists()).toBe(true);
+  });
+
+  it("renders a bootstrap Button with `Submit` text", () => {
+    expect(app.find("Button").exists()).toBe(true);
+    expect(
+      app
+        .find("Button")
+        .render()
+        .text()
+    ).toEqual("Submit");
+  });
 });
